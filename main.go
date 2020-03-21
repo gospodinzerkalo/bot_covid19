11394f146b8484f689e489c582a61dc87885dbd1
Darkhan <180103135@stu.sdu.edu.kz>
2020-03-21 19:20:17 +0600

    make it better

--- main.go
+++ main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"github.com/urfave/cli"
+
 	tb "gopkg.in/tucnak/telebot.v2"
 	"log"
-	"os"
+
 	"time"
 	"./bot"
 )
 
 func main() {
-	app := cli.NewApp()
-	// app.Flags = flags
-	app.Commands = cli.Commands{
-	&cli.Command{
-		Name:   "start",
-		Usage:  "start the bot",
-		Action: StartBot,
-	},
-	}
-	app.Run(os.Args)
-}
-
-func StartBot(d *cli.Context) error {
-
 	b, err := tb.NewBot(tb.Settings{
 		Token:  "731713099:AAHjpa_A8Ewv6CURNqfVqfF7AZ4eVKfwqhM",
 		URL:    "",
@@ -32,7 +18,6 @@ func StartBot(d *cli.Context) error {
 
 	if err != nil {
 		log.Fatal(err)
-		return err
 	}
 
 	end := bot.NewEndpointsFactory()
@@ -42,6 +27,5 @@ func StartBot(d *cli.Context) error {
 	b.Handle(&bot.KeyKz,end.CheckKz(b))
 	b.Handle(&bot.KeyByCountry,end.FindByCountry(b))
 	b.Start()
-
-	return nil
 }
+
