3143b75ece824541e31a85b0f960c18046bbc7a6
Darkhan Mavlenberdiyev <180103135@stu.sdu.edu.kz>
2020-06-24 11:30:03 +0600

    add docker

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/gospodinzerkalo/bot_covid19
+
+go 1.13
+
+require (
+	github.com/urfave/cli v1.22.4 // indirect
+	github.com/urfave/cli/v2 v2.2.0 // indirect
+	gopkg.in/tucnak/telebot.v2 v2.3.2
+)
--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
-
+	"github.com/urfave/cli/v2"
 	tb "gopkg.in/tucnak/telebot.v2"
 	"log"
+	"os"
 
 	"time"
-	"./bot"
+	"github.com/gospodinzerkalo/bot_covid19/bot"
 )
 
+
 func main() {
+	// CLI command for starting APP
+	app := cli.NewApp()
+	app.Commands = cli.Commands{
+		&cli.Command{
+			Name:   "start",
+			Usage:  "start the bot",
+			Action: StartBot,
+		},
+	}
+	app.Run(os.Args)
+
+}
+
+
+func StartBot(d *cli.Context) error {
 	b, err := tb.NewBot(tb.Settings{
 		Token:  "731713099:AAHjpa_A8Ewv6CURNqfVqfF7AZ4eVKfwqhM",
 		URL:    "",
@@ -27,5 +44,7 @@ func main() {
 	b.Handle(&bot.KeyKz,end.CheckKz(b))
 	b.Handle(&bot.KeyByCountry,end.FindByCountry(b))
 	b.Start()
+	return nil
 }
+
 
